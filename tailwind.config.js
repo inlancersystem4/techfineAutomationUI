9d51791b4170d54eeba8e99049b341cedf33a220
inlancersystem4 <132254157+inlancersystem4@users.noreply.github.com>
2024-12-09 11:23:27 +0530

    project  setup

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      screens: {
+        "2xl": "1720px",
+        xl: "1280px",
+      },
+    },
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+      "sans-secondary": ["Oswald", "sans-serif"],
+      serif: ["Ovo", "serif"],
+    },
+    colors: {
+      primary: "hsl(var(--primary) / <alpha-value>)",
+      secondary: "hsl(var(--secondary) / <alpha-value>)",
+      border: "hsl(var(--border) / <alpha-value>)",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
